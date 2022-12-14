54ed02879c6237533fa979b361ed6666a828b86c
gsnorberto <sn_gabriel@outlook.com>
2022-12-14 12:30:01 -0300

    feat: Configurações iniciais do projeto

---
+++ src/Routes/private.routes.js
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default () => {
+    <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+    </BrowserRouter>
+}
