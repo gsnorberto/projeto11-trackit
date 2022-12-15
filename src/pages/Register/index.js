0130c70d18800ec3364ccfdc5ff77b14beb345ef
“gsnorberto” <sn_gabriel@outlook.com>
2022-12-14 21:41:48 -0300

    style: Layout da tela de cadastro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import { Logo, Input, Button, RegisterArea, Btn } from "./styles";
+import logoImg from "../../assets/imgs/logo.png";
+import { mainColor } from "../../constants/colors";
+
 export default () => {
     return(
-        <div></div>
+        <RegisterArea>
+            <Logo src={logoImg} />
+            <Input placeholder="email" />
+            <Input placeholder="senha" />
+            <Input placeholder="nome" />
+            <Input placeholder="foto" />
+            <Button color={mainColor}>Cadastrar</Button>
+            <Link>
+                <Btn color={mainColor}>Já tem uma conta? Faça login!</Btn>
+            </Link>
+        </RegisterArea>
     );
 }
